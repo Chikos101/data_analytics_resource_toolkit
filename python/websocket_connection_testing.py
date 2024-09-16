9e16b3716f7e13dca71997d3ad510503b45e2b4a
Joshi, Aditi (e46568) <Aditi.Joshi@cmegroup.com>
2024-09-16 18:21:16 -0500

    unit test for websocket connection

---
+++ python/websocket_connection_testing.py
@@ -0,0 +1,24 @@
+import unittest
+from unittest.mock import patch, Mock
+from websocket_connection import websocket_request
+import websocket
+from utilities import generate_auth_header
+import ssl
+import json
+config_file = open('config.json')
+config = json.load(config_file)
+class TestWebsocketConnection(unittest.TestCase):
+    @patch('websocket_connection.create_connection')
+    def test_settlements_table(self, mock_get):
+        response_type = websocket._core.WebSocket
+        mock_get.return_value = response_type
+        output = websocket_request()
+        mock_get.assert_called_with(config['websocket_url'],
+                       sslopt={"cert_reqs": ssl.CERT_NONE},
+                       header = [f"Authorization: {generate_auth_header()}"])
+        mock_get.assert_called_once()
+        self.assertIsInstance(output, response_type)
+
+
+if __name__ == '__main__':
+    unittest.main()
