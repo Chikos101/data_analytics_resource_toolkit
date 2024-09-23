7f3ee4611cceea250910670901a9754b14b61ec7
Joshi, Aditi (e46568) <Aditi.Joshi@cmegroup.com>
2024-09-23 16:27:59 -0500

    unit test for websocket connection

--- python/websocket_connection_testing.py
+++ python/websocket_connection_testing.py
@@ -16,7 +16,7 @@ class TestWebsocketConnection(unittest.TestCase):
         mock_websocket.return_value = mock_ws
 
         #simulate real-time headers for helper function
-        mock_gen_auth.return_value = ' Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImIxbE9sa1ptd2x0bUVFbVY2TXU2dl9kRmJ5QSIsInBpLmF0bSI6IjEifQ.eyJzY29wZSI6W10sImNsaWVudF9pZCI6IkFQSV9FNDY1NjhfUFJPRCIsImF1ZCI6IlByb2QiLCJqdGkiOiI3MTRCN1RsTjRvUmpiTGRiIiwic3ViIjoiQVBJX0U0NjU2OF9QUk9EIiwiZXhwIjoxNzI3MTI3MjgyfQ.ZKaSd4ZVmQG1UQ6sHjZi-pl_L9eHTEW1hlinMLBOIx4AqHJmyZ-pZkGRmIZcI2ihiyNEqLE0K8gUwEkx_xPrQbXcpv0c9WhKQQDWbOmffcr54kzpfDvh1RwjTSnJzjyXNSF1_e4jmywzOzim9uqHCm9jD1vznHWkEBeBoLrQfCLNhpC-3lvZwzkvPVkcpHFC5aMZRWIM11pE3DmyZ_91QNKeO9-NUkXwm5w49-SOGFzrjKWmt6ctbeqmEp04XMdMTOXIKlmp8GRE23Pqcsh6ozMHKhSr8I-JRqHGXw9mYXhowRzXv1YgUBIKF5uC6iwUXWpsDFMgHR8dIZg655oUfQ'
+        mock_gen_auth.return_value = ' Bearer access token'
 
         # call the function for testing
         output = websocket_request(config)
@@ -24,7 +24,7 @@ class TestWebsocketConnection(unittest.TestCase):
         # assert the websocket connection was created with real-time headers
         mock_websocket.assert_called_with(config['websocket_url'],
                        sslopt={"cert_reqs": ssl.CERT_NONE},
-                       header = [f"Authorization: Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImIxbE9sa1ptd2x0bUVFbVY2TXU2dl9kRmJ5QSIsInBpLmF0bSI6IjEifQ.eyJzY29wZSI6W10sImNsaWVudF9pZCI6IkFQSV9FNDY1NjhfUFJPRCIsImF1ZCI6IlByb2QiLCJqdGkiOiI3MTRCN1RsTjRvUmpiTGRiIiwic3ViIjoiQVBJX0U0NjU2OF9QUk9EIiwiZXhwIjoxNzI3MTI3MjgyfQ.ZKaSd4ZVmQG1UQ6sHjZi-pl_L9eHTEW1hlinMLBOIx4AqHJmyZ-pZkGRmIZcI2ihiyNEqLE0K8gUwEkx_xPrQbXcpv0c9WhKQQDWbOmffcr54kzpfDvh1RwjTSnJzjyXNSF1_e4jmywzOzim9uqHCm9jD1vznHWkEBeBoLrQfCLNhpC-3lvZwzkvPVkcpHFC5aMZRWIM11pE3DmyZ_91QNKeO9-NUkXwm5w49-SOGFzrjKWmt6ctbeqmEp04XMdMTOXIKlmp8GRE23Pqcsh6ozMHKhSr8I-JRqHGXw9mYXhowRzXv1YgUBIKF5uC6iwUXWpsDFMgHR8dIZg655oUfQ"])
+                       header = [f"Authorization: Bearer access token"])
         
         # assert the helper function was called with the correct real-time arguments
         mock_gen_auth.asser_called_with(config)
