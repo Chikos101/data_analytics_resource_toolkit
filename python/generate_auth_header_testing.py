305c0086ea17bc93c3d777e9dd041ddb89b04045
Joshi, Aditi (e46568) <Aditi.Joshi@cmegroup.com>
2024-09-23 14:56:33 -0500

    unit tests

---
+++ python/generate_auth_header_testing.py
@@ -0,0 +1,22 @@
+from utilities import generate_auth_header
+import unittest
+from unittest.mock import patch, Mock
+import requests
+import json
+
+config_file = open('config.json')
+config = json.load(config_file)
+
+
+class TestGenerateAuthHeader(unittest.TestCase):
+    @patch('requests.post')
+    def test_generate_auth_header(self, mock_get):
+        response = generate_auth_header(config)
+        mock_get.asser_called_with(config["auth_url"], {'grant_type': 'client_credentials'},
+                              (config["client_id"], config["client_secret"]))
+        self.assertIsInstance(response, str)
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
--- python/websocket_connection_testing.py
+++ python/websocket_connection_testing.py
@@ -8,15 +8,15 @@ import json
 config_file = open('config.json')
 config = json.load(config_file)
 class TestWebsocketConnection(unittest.TestCase):
-    @patch('websocket_connection.create_connection')
-    def test_settlements_table(self, mock_get):
+    @patch('websocket.create_connection')
+    def test_websocket_connection(self,  mock_get):
         response_type = websocket._core.WebSocket
         mock_get.return_value.type = response_type
-        output = websocket_request()
+        output = websocket_request(config)
+        print(output)
         mock_get.assert_called_with(config['websocket_url'],
                        sslopt={"cert_reqs": ssl.CERT_NONE},
-                       header = [f"Authorization: {generate_auth_header()}"])
-        mock_get.assert_called_once()
+                       header = [f"Authorization: {generate_auth_header(config)}"])
         self.assertIsInstance(output, response_type)
 
 
