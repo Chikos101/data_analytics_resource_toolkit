b03b4cd64e7a6f8c5f0d81fc3ac2d81e9f508969
Yuan, Jeffrey <Jeffrey.Yuan@cmegroup.com>
2024-11-05 14:19:46 -0600

    Data range fix

--- src/main/sql/VWAP.sql
+++ src/main/sql/VWAP.sql
@@ -5,7 +5,7 @@ DECLARE run_symbols ARRAY <STRING>;
 --Parameters:
 -----------------------------------------------------------------
 SET intervals = "minute"; -- Desired granularity (acceted values ['day', 'hour', 'minute'])
-SET date_range = [DATE("2024-08-12"), DATE("2024-08-13")]; -- Desired date of interest
+SET date_range = [DATE("2024-08-12"), DATE("2024-08-13")]; -- Desired date range
 SET run_symbols = ["ESU4"]; -- Desired symbols
 -----------------------------------------------------------------
 WITH full_obr AS(
